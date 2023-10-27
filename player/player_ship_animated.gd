b90c9dfe88caa50d3d4ffe9e5f68385e2a1c227c
CSpiegelhalter <spiegelhaltercurt@gmail.com>
2023-10-27 12:30:57 -0500

    Added a test enemy that just follows player. Also added input controlers for keyboard

--- player/player_ship_animated.gd
+++ player/player_ship_animated.gd
@@ -6,27 +6,21 @@ var Mini_Rocket = load("res://projectiles/mini_rocket.tscn")
 var speed = 400
 var rotation_speed = PI
 
+func _ready():
+	# Set a global reference to our player (see Globals.gd)
+	Globals.set("player", self)
+
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	var movementVector = Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
+	
 	var rotationDirection = 0
 	var joypadPosition = Input.get_vector("ui_left", "ui_right", "ui_down", "ui_up")
 		
 	if joypadPosition != Vector2(0,0):
 		print(joypadPosition)
 		
-	position += movementVector * speed * delta
-	rotation += rotationDirection * rotation_speed * delta
-	
-	# Rotation Inputs
-	if Input.is_action_pressed("player_turn_left"):
-		rotation -= rotation_speed  * delta
-		animation = "rotate_left"
-	elif Input.is_action_pressed("player_turn_right"):
-		rotation += rotation_speed  * delta
-		animation = "rotate_right"
-	else:
-		animation = "idle"
+	handle_rotation(delta)
+	handle_movment(delta)
 		
 		
 func _on_fire_button():
@@ -37,3 +31,32 @@ func _input(event):
 	if event is InputEventJoypadButton:
 		if event.is_action_pressed("player_shoot"):
 			shoot.emit(Mini_Rocket, rotation, $ProjectileStart.global_position);
+
+func handle_movment(delta):
+	var movementVector = Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
+	position += movementVector * speed * delta
+	if !Input.is_anything_pressed():
+		return
+	if Input.is_action_pressed("player_move_up"):
+		position.y -= speed * delta
+	elif Input.is_action_pressed("player_move_down"):
+		position.y += speed * delta
+	elif Input.is_action_pressed("player_move_right"):
+		position.x += speed * delta
+	elif Input.is_action_pressed("player_move_left"):
+		position.x -= speed * delta
+
+	
+func handle_rotation(delta):
+	# Rotation Inputs
+	if !Input.is_anything_pressed():
+		animation = "idle"
+		return
+	if Input.is_action_pressed("player_turn_left"):
+		rotation -= rotation_speed * delta
+		animation = "rotate_left"
+	elif Input.is_action_pressed("player_turn_right"):
+		rotation += rotation_speed * delta
+		animation = "rotate_right"
+
+		
